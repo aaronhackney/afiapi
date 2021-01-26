e5e3a8c8f8d224bddfe24d4c22d5238789356403
aaronhackney <ahack210@gmail.com>
2021-01-26 17:18:16 -0600

    finally remembered

--- nginx/nginx/docker-entrypoint.sh
+++ nginx/nginx/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eu
-envsubst '${NGINX_HOST_NAME} ${NGINX_PORT} ${BACKEND_HOSTNAME} ${BACKEND_PORT} ${TLS_CERT_FILE} ${TLS_KEY_FILE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${TLS_CERT_PATH} ${NGINX_HOST_NAME} ${NGINX_PORT} ${BACKEND_HOSTNAME} ${BACKEND_PORT} ${TLS_CERT_FILE} ${TLS_KEY_FILE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec "$@"
