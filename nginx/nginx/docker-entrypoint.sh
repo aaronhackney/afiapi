7459cd9635fc048859f920c423cd11484f88cc4c
aaronhackney <ahack210@gmail.com>
2021-02-01 21:05:46 -0600

    unneeded shell script

--- nginx/nginx/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-envsubst '${TLS_CERT_PATH} ${NGINX_HOST_NAME} ${NGINX_PORT} ${BACKEND_HOSTNAME} ${BACKEND_PORT} ${TLS_CERT_FILE} ${TLS_KEY_FILE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-exec "$@"
